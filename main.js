ae83d91997dacb6cc22b3456d7312d66c315c185
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-01 15:18:45 -0400

    finished test

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 function fizzy(num) {
+  let response = " "
+  
+  if (num % 3 ===0 && num % 5 === 0){
+    response ="FizzBuzz"
+    
+    
+  }else if(num%5===0 && num%3 !==0){
+    response = 'Buzz'
 
+  }else if (num % 3===0 && num % 5!==0){
+    response = "Fizz"
+  } else if (num % 3 !==0 && num % 5!==0){
+    response = num
+  }
+  
+  
+  
+  
+  return response
 }
 
 module.exports = {
